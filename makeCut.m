eaeaeadfaa1251249dc3bca0cc15489604ff002b
SanZamoyski <san.zamoyski@wp.pl>
2020-05-17 19:41:00 +0200

    Delete makeCut.m

--- makeCut.m
+++
@@ -1,9 +0,0 @@
-basenames = {"3W9X"; "3W9Y"; "3M9X"; "3M9Y"; "3H9X"; "3H9Y"};
-
-tic
-
-for sample=1:size(basenames)(1)
-  printf("%s: ", basenames{sample});
-  cutForSamples(basenames{sample});
-  toc
-endfor
